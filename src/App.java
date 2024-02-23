8d8c906b06f00327230476c4a3fd0b75b73e4a1a
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-02-23 19:21:09 -0300

    feat: Adding all code to create a new user on controller class

--- src/App.java
+++ src/App.java
@@ -1,37 +1,8 @@
-import java.util.Scanner;
-
-import entities.User;
+import controller.CreateUserController;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("================== New User ==================");
-
-        System.out.print("Email: ");
-        String email = scanner.nextLine();
-        
-        System.out.print("Name: ");
-        String name = scanner.nextLine();
-        
-        System.out.print("Addres: ");
-        String addres = scanner.nextLine();
-        
-        System.out.print("Age: ");
-        Number age = scanner.nextInt();
-        
-        User u = new User();
-
-        u.setId();
-        u.setEmail(email);
-        u.setName(name);
-        u.setAge(age);
-        u.setAddres(addres);
-        
-        System.out.println("UserID: " + u.getId());
-        System.out.println("Email: " + u.getEmail());
-        System.out.println("Name: " + u.getName());
-        System.out.println("Age: " + u.getAge());
-        System.out.println("Address: " + u.getAddres());
+        CreateUserController creatUserControler = new CreateUserController();
+        creatUserControler.createUser();
     }
 }
---
+++ src/controller/CreateUserController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entities.User;
+
+public class CreateUserController {
+    public static void createUser() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<User> users = new ArrayList<User>();
+
+        System.out.println("========= Create User =========");
+    
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Addres: ");
+        String addres = scanner.nextLine();
+
+        System.out.print("Age: ");
+        Number age = scanner.nextInt();
+
+        User u = new User();
+
+        u.setId();
+        u.setEmail(email);
+        u.setName(name);
+        u.setAge(age);
+        u.setAddres(addres);
+
+        users.add(u);
+    }
+}
