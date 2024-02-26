e2611fe9b8077423505b2502766586bc382c6f64
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-02-26 16:10:13 -0300

    wip: intance of the create user controller

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 import controller.CreateUserController;
+import controller.CreateEventController;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
